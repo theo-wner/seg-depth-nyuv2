821bcc540e60d1d04dd156c8fdd6a4f43801eb30
Theodor Kapler <theodor.kapler@student.kit.edu>
2023-11-23 16:59:25 +0100

    augmentation test gestartet

--- exp2.sh
+++ exp2.sh
@@ -4,12 +4,42 @@
 # Segmentation
 ##########################################################################################
 
-python train.py --task seg --name seg/exp1-backbone --version b0 --devices 1 --backbone b0
+python train.py --task seg --name seg/exp2-augmentation --version fff --augmentations none
+
+python train.py --task seg --name seg/exp2-augmentation --version tff --augmentations rand_scale
+
+python train.py --task seg --name seg/exp2-augmentation --version ftf --augmentations rand_flip
+
+python train.py --task seg --name seg/exp2-augmentation --version fft --augmentations colorjitter
+
+python train.py --task seg --name seg/exp2-augmentation --version ttf --augmentations rand_scale rand_flip
+
+python train.py --task seg --name seg/exp2-augmentation --version tft --augmentations rand_scale colorjitter
+
+python train.py --task seg --name seg/exp2-augmentation --version ftt --augmentations rand_flip colorjitter
+
+python train.py --task seg --name seg/exp2-augmentation --version ttt --augmentations rand_scale rand_flip colorjitter
 
 
 ##########################################################################################
 # Depth Estimation
 ##########################################################################################
+
+python train.py --task depth --name depth/exp2-augmentation --version fff --augmentations none
+
+python train.py --task depth --name depth/exp2-augmentation --version tff --augmentations rand_scale
+
+python train.py --task depth --name depth/exp2-augmentation --version ftf --augmentations rand_flip
+
+python train.py --task depth --name depth/exp2-augmentation --version fft --augmentations colorjitter
+
+python train.py --task depth --name depth/exp2-augmentation --version ttf --augmentations rand_scale rand_flip
+
+python train.py --task depth --name depth/exp2-augmentation --version tft --augmentations rand_scale colorjitter
+
+python train.py --task depth --name depth/exp2-augmentation --version ftt --augmentations rand_flip colorjitter
+
+python train.py --task depth --name depth/exp2-augmentation --version ttt --augmentations rand_scale rand_flip colorjitter
 
 
 
@@ -17,6 +47,21 @@ python train.py --task seg --name seg/exp1-backbone --version b0 --devices 1 --b
 # Simultaneous Segmentation and Depth Estimation
 ##########################################################################################
 
+python train.py --task segdepth --name segdepth/exp2-augmentation --version fff --augmentations none
+
+python train.py --task segdepth --name segdepth/exp2-augmentation --version tff --augmentations rand_scale
+
+python train.py --task segdepth --name segdepth/exp2-augmentation --version ftf --augmentations rand_flip
+
+python train.py --task segdepth --name segdepth/exp2-augmentation --version fft --augmentations colorjitter
+
+python train.py --task segdepth --name segdepth/exp2-augmentation --version ttf --augmentations rand_scale rand_flip
+
+python train.py --task segdepth --name segdepth/exp2-augmentation --version tft --augmentations rand_scale colorjitter
+
+python train.py --task segdepth --name segdepth/exp2-augmentation --version ftt --augmentations rand_flip colorjitter
+
+python train.py --task segdepth --name segdepth/exp2-augmentation --version ttt --augmentations rand_scale rand_flip colorjitter
 
 echo "All training runs finished."
 
