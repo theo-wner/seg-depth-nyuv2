c48d322399a844d01bf26d4b1135f53b55ce1fe3
Theodor Kapler <theodor.kapler@student.kit.edu>
2023-11-29 14:10:35 +0100

    m

--- exp3.sh
+++ exp3.sh
@@ -4,27 +4,27 @@
 # Segmentation
 ##########################################################################################
 
-python train.py --task seg --name seg/exp3-lr --version e4 --augmentations rand_scale colorjitter --learning_rate 6e-4
+python train.py --task seg --name seg/exp3-lr --version e4 --learning_rate 6e-4
 
-python train.py --task seg --name seg/exp3-lr --version e6 --augmentations rand_scale colorjitter --learning_rate 6e-6
+python train.py --task seg --name seg/exp3-lr --version e6 --learning_rate 6e-6
 
 
 ##########################################################################################
 # Depth Estimation
 ##########################################################################################
 
-python train.py --task depth --name depth/exp3-lr --version e4 --augmentations rand_flip colorjitter --learning_rate 6e-4
+python train.py --task depth --name depth/exp3-lr --version e4 --learning_rate 6e-4
 
-python train.py --task depth --name depth/exp3-lr --version e6 --augmentations rand_flip colorjitter --learning_rate 6e-6
+python train.py --task depth --name depth/exp3-lr --version e6 --learning_rate 6e-6
 
 
 ##########################################################################################
 # Simultaneous Segmentation and Depth Estimation
 ##########################################################################################
 
-python train.py --task segdepth --name segdepth/exp3-lr --version e4 --augmentations beste --learning_rate 6e-4
+python train.py --task segdepth --name segdepth/exp3-lr --version e4 --learning_rate 6e-4
 
-python train.py --task segdepth --name segdepth/exp3-lr --version e6 --augmentations beste --learning_rate 6e-6
+python train.py --task segdepth --name segdepth/exp3-lr --version e6 --learning_rate 6e-6
 
 echo "All training runs finished."
 
