168bd374b3969580eebe8c935066a0d2c9500538
Theodor Kapler <theodor.kapler@student.kit.edu>
2023-12-01 16:27:22 +0100

    changed exp3.sh

--- exp3.sh
+++ exp3.sh
@@ -1,32 +1,40 @@
 #!/bin/bash
 
 ##########################################################################################
+# Log Errors
+##########################################################################################
+logfile="exp3_log.txt"
+
+##########################################################################################
 # Segmentation
 ##########################################################################################
 
-python train.py --task seg --name seg/exp3-lr --version e4 --learning_rate 6e-4
+python train.py --task seg --name seg/exp3-lr --version e4 --learning_rate 6e-4 1>/dev/null 2>> "$logfile"
+echo "-------------------------------------------------------------------------" >> "$logfile" 2>&1
 
-python train.py --task seg --name seg/exp3-lr --version e6 --learning_rate 6e-6
+#python train.py --task seg --name seg/exp3-lr --version e6 --learning_rate 6e-6 1>/dev/null 2>> "$logfile"
+#echo "-------------------------------------------------------------------------" >> "$logfile" 2>&1
 
 
 ##########################################################################################
 # Depth Estimation
 ##########################################################################################
 
-python train.py --task depth --name depth/exp3-lr --version e4 --learning_rate 6e-4
+#python train.py --task depth --name depth/exp3-lr --version e4 --learning_rate 6e-4 1>/dev/null 2>> "$logfile"
+#echo "-------------------------------------------------------------------------" >> "$logfile" 2>&1
 
-python train.py --task depth --name depth/exp3-lr --version e6 --learning_rate 6e-6
+#python train.py --task depth --name depth/exp3-lr --version e6 --learning_rate 6e-6 1>/dev/null 2>> "$logfile"
+#echo "-------------------------------------------------------------------------" >> "$logfile" 2>&1
 
 
 ##########################################################################################
 # Simultaneous Segmentation and Depth Estimation
 ##########################################################################################
 
-python train.py --task segdepth --name segdepth/exp3-lr --version e4 --learning_rate 6e-4
+python train.py --task segdepth --name segdepth/exp3-lr --version e4 --learning_rate 6e-4 1>/dev/null 2>> "$logfile"
+echo "-------------------------------------------------------------------------" >> "$logfile" 2>&1
 
-python train.py --task segdepth --name segdepth/exp3-lr --version e6 --learning_rate 6e-6
-
-echo "All training runs finished."
+#python train.py --task segdepth --name segdepth/exp3-lr --version e6 --learning_rate 6e-6 1>/dev/null 2>> "$logfile"
 
 
 
