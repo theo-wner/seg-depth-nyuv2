cbe49f6156bb82cfd1dc4b18c64fb880bdfa4e54
Theodor Kapler <theodor.kapler@student.kit.edu>
2023-12-02 07:51:37 +0100

    added lr monitor

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import config
 from dataset import NYUv2DataModule
 from model import SegFormer, DepthFormer, SegDepthFormer
 import transformers
+from pytorch_lightning.callbacks import LearningRateMonitor
 
 """
 Trains the model
@@ -27,8 +28,11 @@ if __name__ == '__main__':
     elif config.TASK == 'segdepth':
         model = SegDepthFormer()
 
+    # Erstellen Sie eine Instanz von LearningRateMonitor
+    lr_monitor = LearningRateMonitor(logging_interval='step')
+
     # Initialize the trainer
-    trainer = pl.Trainer(logger=logger, max_epochs=config.NUM_EPOCHS, accelerator='gpu', precision=config.PRECISION, devices=config.DEVICES)
+    trainer = pl.Trainer(logger=logger, max_epochs=config.NUM_EPOCHS, accelerator='gpu', precision=config.PRECISION, devices=config.DEVICES, callbacks=[lr_monitor])
 
     # Train the model
     trainer.fit(model, data_module, ckpt_path=config.CHECKPOINT)
